05e6c45e3f53297fe5fc9ea94df39add902aef33
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2015-08-17 12:32:38 +0200

    fix

--- imgur-install.sh
+++ imgur-install.sh
@@ -1,3 +1,3 @@
-curl https://raw.githubusercontent.com/jomo/imgur-screenshot/master/imgur-screenshot.sh >> /usr/bin/imgur
+curl https://raw.githubusercontent.com/jomo/imgur-screenshot/master/imgur-screenshot.sh > /usr/bin/imgur
 chmod +x /usr/bin/imgur
 alias imgur='/usr/bin/./imgur'
